af3b44fd049b2986c14efcb2a8b52f4f8443a32c
gilsonrusso <gilsonrusso@outlook.com>
2021-03-02 22:53:39 -0400

    styled cards

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,158 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar app color="orange darken-3" dark>
+      <v-row>
+        <v-col>
+          <v-divider class="mx-4" vertical></v-divider>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="344">
+            <v-card-actions>
+              <v-btn color="orange lighten-2" text> Explore </v-btn>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+              <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{
+                  show ? "mdi-chevron-up" : "mdi-chevron-down"
+                }}</v-icon>
+              </v-btn>
+            </v-card-actions>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  <a href="#">Sair</a>
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-divider vertical></v-divider>
+        </v-col>
+      </v-row>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main class="mt-5">
+      <v-container>
+        <v-row>
+          <v-col cols="4">
+            <v-flex>
+              <v-form>
+                <v-text-field
+                  outlined
+                  label="Busque por nomes ou emails"
+                  append-icon="mdi-magnify"
+                ></v-text-field>
+              </v-form>
+            </v-flex>
+          </v-col>
+          <v-col cols="4"></v-col>
+          <v-col cols="4" class="d-flex">
+            <v-col cols="6" class="d-flex">
+              <span class="pr-2 py-2"><strong>Filtros:</strong></span>
+              <v-select :items="items" label="Todos" dense solo></v-select>
+            </v-col>
+            <v-col cols="6">
+              <v-btn>
+                <v-icon>mdi-plus</v-icon>
+                Adicionar</v-btn
+              >
+            </v-col>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col v-for="card in listData" :key="card" cols="12" sm="3" md="3">
+            <Card :data="card" />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import Card from "./components/Card";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Card,
   },
 
   data: () => ({
-    //
+    show: false,
+    items: ["Foo", "Bar", "Fizz", "Buzz"],
+    listData: [
+      {
+        name: "Fulano de Souza",
+        lastReview: "12/10/2020",
+        image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        email: "fulano@gmail.com",
+        age: "30",
+        phone: "(11) 99844-3540",
+      },
+      {
+        name: "Ciclano Moura",
+        lastReview: "12/10/2020",
+        image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        email: "ciclano@gmail.com",
+        age: "25",
+        phone: "(11) 99844-3540",
+      },
+      {
+        name: "Beltrano de lima",
+        lastReview: "12/10/2020",
+        image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        email: "beltrano@gmail.com",
+        age: "38",
+        phone: "(11) 99844-3540",
+      },
+      {
+        name: "Carlos Ferraz",
+        lastReview: "12/10/2020",
+        image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        email: "carlos@gmail.com",
+        age: "18",
+        phone: "(11) 99844-3540",
+      },
+      {
+        name: "Marcio de Castro",
+        lastReview: "12/10/2020",
+        image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        email: "marcio@gmail.com",
+        age: "45",
+        phone: "(11) 99844-3540",
+      },
+      {
+        name: "Silvana Silva",
+        lastReview: "12/10/2020",
+        image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        email: "silvana@gmail.com",
+        age: "53",
+        phone: "(11) 99844-3540",
+      },
+    ],
+    cards: [
+      {
+        title: "Pre-fab homes",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 4,
+      },
+      {
+        title: "Favorite road trips",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 4,
+      },
+      {
+        title: "Best airlines",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 4,
+      },
+    ],
   }),
 };
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card class="pa-2 rounded" elevation="3" min-width="280px">
+    <v-row>
+      <v-card-actions>
+        <v-list-item class="grow">
+          <v-list-item-avatar color="grey darken-3">
+            <v-img class="elevation-6" alt="" :src="data.image"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title
+              ><strong>{{ data.name }}</strong></v-list-item-title
+            >
+            <v-list-item-title
+              ><small class="text-details"
+                >Última avaliação: {{ data.lastReview }}</small
+              ></v-list-item-title
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-card-actions>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row class="py-2 px-2 align-center">
+      <v-col cols="8">
+        <span class="d-block"
+          ><small class="text-details">{{ data.email }}</small></span
+        >
+        <span class="d-block"
+          ><small class="text-details">{{ data.age }} {{ data.phone }}</small></span
+        >
+      </v-col>
+      <v-col cols="4">
+        <v-row class="justify-space-around">
+          <v-btn rounded x-small height="40" width="40" color="grey lighten-5">
+            <v-icon color="blue accent-4"> mdi-pencil </v-icon>
+          </v-btn>
+          <v-btn
+            rounded
+            x-small
+            height="40"
+            width="40"
+            depressed
+            color="transparent"
+          >
+            <v-icon color="blue accent-4"> mdi-dots-horizontal </v-icon>
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["data"],
+};
+</script>
+
+<style>
+.text-details {
+  color: var(--font-color);
+  font-size: 0.8rem;
+}
+</style>
