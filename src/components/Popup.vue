57d3d6d9b150bf4ac33b082b603836a5141e5209
gilsonrusso <gilsonrusso@outlook.com>
2021-03-04 15:27:05 -0400

    add validation in fields

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,17 +5,20 @@
         <span class="headline">Cadastro de Aluno</span>
       </v-card-title>
       <v-card-text>
-        <v-container>
+        <v-form ref="form" v-model="valid" lazy-validation>
           <v-row>
             <v-col cols="12">
               <v-text-field
                 label="Nome*"
+                :rules="nameRules"
                 v-model="user.name"
                 required
               ></v-text-field>
             </v-col>
             <v-col cols="12">
               <v-text-field
+                type="email"
+                :rules="rulesEmail"
                 label="Email*"
                 required
                 v-model="user.email"
@@ -25,7 +28,8 @@
               <v-text-field
                 label="Idade*"
                 v-model="user.age"
-                max="80"
+                :rules="idadeRules"
+                max="100"
                 min="10"
                 type="number"
                 required
@@ -34,7 +38,9 @@
             <v-col cols="12" sm="6">
               <v-text-field
                 label="Telefone*"
+                :rules="phoneRules"
                 v-model="user.phone"
+                counter="11"
                 type="text"
                 required
               ></v-text-field>
@@ -47,7 +53,7 @@
               ></v-switch>
             </v-col>
           </v-row>
-        </v-container>
+        </v-form>
         <small>*indicação de campos obrigatórios</small>
       </v-card-text>
       <v-card-actions>
@@ -55,7 +61,7 @@
           ><strong>Cancelar</strong></v-btn
         >
         <v-spacer></v-spacer>
-        <v-btn class="btn btn-save" text @click="save()">{{
+        <v-btn :disabled="!valid" class="btn btn-save" text @click="save()">{{
           user.id ? "Atualizar" : "Salvar"
         }}</v-btn>
       </v-card-actions>
@@ -68,14 +74,56 @@ export default {
   name: "popup-component",
   props: ["user", "isOpen"],
   data() {
-    return {};
+    return {
+      valid: true,
+      rules: [],
+      rulesEmail: [
+        (value) => !!value || "Required.",
+        (value) =>
+          (value || "").length <= 25 || "Número máximo de caracterer é 25",
+        (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Formato de e-mail inválido!.";
+        },
+      ],
+      nameRules: [
+        (value) => !!value || "Required.",
+        (value) =>
+          (value || "").length <= 37 || "Número máximo de caracterer é 37",
+        (value) => {
+          const pattern = /^[a-zA-Z]+[\s|-]?[a-zA-Z]+[\s|-]?[a-zA-Z]+$/;
+          return pattern.test(value) || "Nome Inválido!.";
+        },
+      ],
+      phoneRules: [
+        (value) => !!value || "Required.",
+        (value) =>
+          (value || "").length <= 11 || "Número máximo de caracterer é 11",
+        (value) => {
+          const pattern = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+          return pattern.test(value) || "Nome Inválido!.";
+        },
+      ],
+      idadeRules: [
+        (value) => !!value || "Required.",
+        (value) =>
+          (value || "").length <= 2 || "Número máximo de caracterer é 02",
+      ],
+    };
   },
   methods: {
     save() {
       this.$emit("save");
+      this.$refs.form.reset();
     },
     cancel() {
+      this.$refs.form.reset();
       this.$emit("closeDialog");
+    },
+  },
+  computed: {
+    disableBtn: function () {
+      return this.$rules > 0 ? true : false;
     },
   },
 };
